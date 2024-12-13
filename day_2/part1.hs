475477e6d225aeeec97cd6aa430b5e78e4ae3673
clement <clement.jvdm@gmail.com>
2024-12-14 00:21:54 +0100

    day 2: cleaner part1 code

--- day_2/part1.hs
+++ day_2/part1.hs
@@ -4,12 +4,9 @@ import System.IO
 main = do
   contents <- lines <$> readFile "input.txt"
   let levels :: [[Int]] = map read . words <$> contents
-  let safe = length $ filter safeCheck levels
+  let safe = length $ filter (\level -> safeCheck level || safeCheck (reverse level)) levels
   print safe 
 
 safeCheck :: [Int] -> Bool
-safeCheck xs = orderCheck && diffCheck
-  where
-    diffList = zipWith (\x y -> abs (x-y)) xs (tail xs)
-    diffCheck = (maximum diffList <= 3) && (minimum diffList >= 1)
-    orderCheck = sort xs == xs || sort xs == reverse xs
+safeCheck [x,y] = x < y && (y-x) >= 1 && (y-x) <= 3
+safeCheck (x:y:xs) = x < y && (y-x) >= 1 && (y-x) <= 3 && safeCheck (y:xs)
