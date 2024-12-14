271f0112935bf20e1409f0836d59196177f06e96
clement <clement.jvdm@gmail.com>
2024-12-14 04:05:02 +0100

    day 3: improved code quality for part 1

--- day_3/part1.hs
+++ day_3/part1.hs
@@ -1,10 +1,9 @@
 import Text.Regex.TDFA ((=~))
-import Text.Regex (matchRegex)
 
-main = do 
+main = do
   contents <- readFile "input.txt"
   let reg = contents =~ "mul\\(([0-9]+),([0-9]+)\\)" :: [[String]]
   print $ calc reg
 
 calc :: [[String]] -> Int
-calc xs = sum [ (read x)*(read y) | [_,x,y] <- xs ]
+calc xs = sum [ read x*read y | [_,x,y] <- xs ]
